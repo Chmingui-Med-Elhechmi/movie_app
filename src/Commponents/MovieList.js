2579ed086de7c34ca96f5e9ea504859f27a5d209
chmingui med elhechmi <med.chmingui@gmail.com>
2022-02-02 11:04:01 +0100

    first commit

--- src/Commponents/MovieList.js
+++ src/Commponents/MovieList.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import MovieCard from './MovieCard';
+import React, { useState } from "react";
+import Add from "./Add";
+import MovieCard from "./MovieCard";
 
-function MovieList(props) {
+
+function MovieList({ movies,addMovie }) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
-    {props.movies.map((movie) => (
-      <div className='image-container'>
-        <MovieCard movies={movie}/>
-      </div>
-    ))}
+      <button onClick={handleShow}>Add</button>
+      <Add show={show} handleClose={handleClose} addMovie={addMovie}/>
+      
+
+      {movies.map((movie) => (
+        <div className="image-container">
+          <MovieCard movies={movie} />
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
 export default MovieList;
